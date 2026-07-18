691bdbedd8c353f7baeffb399004c30e6df7fcf8
Jason Mack <44202037+jmack-dev@users.noreply.github.com>
2026-07-18 18:19:34 +1000

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        clean = []
+        for c in s:
+            if 48 <= ord(c) <= 57 or 65 <= ord(c) <= 90:
+                clean.append(c)
+            if 97 <= ord(c) <= 122:
+                clean.append( chr(ord(c) - 32))
+        return clean == list(reversed(clean))
